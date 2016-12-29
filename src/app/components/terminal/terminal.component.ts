4c595dd77e490358579c9d0f37706e1638d38294
Jeremy <aftem@usc.edu>
2016-12-29 03:06:56 -0600

    Basic design finished.

---
+++ src/app/components/terminal/terminal.component.ts
@@ -0,0 +1,16 @@
+import { Component,
+         OnInit } from '@angular/core';
+
+@Component({
+  selector: 'terminal',
+  templateUrl: '../../components/terminal/terminal.component.html',
+  styleUrls: ['../../components/terminal/terminal.component.css']
+})
+export class TerminalComponent implements OnInit {
+
+    constructor() {    }
+
+    ngOnInit() {
+
+    }
+}
